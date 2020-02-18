fc097e1a6b171dfc55942cc0dfbd5f6cb58984ad
kperkins22 <kaitlynperkins22@gmail.com>
2020-02-17 23:04:58 -0500

    Camera Flip

---
+++ GAME399_ARAirHockey/Assets/Scripts/CameraFlip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFlip : MonoBehaviour
+{
+    public Camera thecamera;
+    public bool flipHorizontal;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flipHorizontal = true;
+    }
+
+  
+    void Update()
+    {
+        thecamera.ResetWorldToCameraMatrix();
+        thecamera.ResetProjectionMatrix();
+        Vector3 scale = new Vector3(flipHorizontal ? -1 : 1, 1, 1);
+        thecamera.projectionMatrix = thecamera.projectionMatrix * Matrix4x4.Scale(scale);
+    }
+}
