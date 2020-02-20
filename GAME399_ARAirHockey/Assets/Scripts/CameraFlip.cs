ceb79b369ec4dc810bc1c050bfd75ae4541615c4
kperkins22 <kaitlynperkins22@gmail.com>
2020-02-19 19:33:30 -0500

    more camera action

--- GAME399_ARAirHockey/Assets/Scripts/CameraFlip.cs
+++ GAME399_ARAirHockey/Assets/Scripts/CameraFlip.cs
@@ -22,4 +22,13 @@ public class CameraFlip : MonoBehaviour
         Vector3 scale = new Vector3(flipHorizontal ? -1 : 1, 1, 1);
         thecamera.projectionMatrix = thecamera.projectionMatrix * Matrix4x4.Scale(scale);
     }
+
+       void OnPreRender()
+    {
+        GL.invertCulling = flipHorizontal;
+    }
+    void OnPostRender()
+    {
+        GL.invertCulling = false;
+    }
 }
