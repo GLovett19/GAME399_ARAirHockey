613acce75e5aca56fccb5c3eb1f700d7ed6f8db7
VashSource <truongqhuynh89@gmail.com>
2020-02-27 22:37:51 -0500

    Puck hitting goal works, as well as puck hitting walls and puck hitting paddle.

--- GAME399_ARAirHockey/Assets/Scripts/TriggerTesting.cs
+++ GAME399_ARAirHockey/Assets/Scripts/TriggerTesting.cs
@@ -7,18 +7,23 @@ public class TriggerTesting : MonoBehaviour
     //Self Populating Components
     ScoreManager sm_manager;
 
+    public AudioClip explosion;
+    AudioSource audioSource;
+
     //public fields
     public string playerGoal; // should only be Player1 or Player2
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         sm_manager = FindObjectOfType<ScoreManager>();
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<PuckMovement>())
-        {         
+        if (other.GetComponent<PuckMovement>())
+        {
+            audioSource.PlayOneShot(explosion, 0.7f);
             sm_manager.SetScore(1, playerGoal);
         }
     }
